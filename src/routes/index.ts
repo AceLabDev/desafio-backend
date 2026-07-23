3b0790db9a0d4a5e8f497217face00a30b375f98
Gabriel Rojas <gabrandueza@gmail.com>
2026-07-23 19:17:21 -0400

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import authRoutes from "../modules/auth/auth.routes";
-// más adelante:
-// import monedaRoutes from "../modules/moneda/moneda.routes.js";
+import monedaRoutes from "../modules/moneda/moneda.routes";
 // import criptomonedaRoutes from "../modules/criptomoneda/criptomoneda.routes.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
-// router.use("/moneda", monedaRoutes);
+router.use("/moneda", monedaRoutes);
 // router.use("/criptomoneda", criptomonedaRoutes);
 
 export default router;
