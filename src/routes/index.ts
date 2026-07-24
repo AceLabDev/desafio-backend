a14c7edb0ee98fd7a2cb15e4c1e8d771d244bdc7
Gabriel Rojas <gabrandueza@gmail.com>
2026-07-23 22:29:46 -0400

    Update: Actualizar rutas de la API

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import authRoutes from "../modules/auth/auth.routes";
 import monedaRoutes from "../modules/moneda/moneda.routes";
-// import criptomonedaRoutes from "../modules/criptomoneda/criptomoneda.routes.js";
+import criptomonedaRoutes from "../modules/criptomoneda/criptomoneda.routes";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/moneda", monedaRoutes);
-// router.use("/criptomoneda", criptomonedaRoutes);
+router.use("/criptomoneda", criptomonedaRoutes);
 
 export default router;
